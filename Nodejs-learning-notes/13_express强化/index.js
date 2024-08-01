5336b4b41f63823b3e6f0458d148e872242bff15
tyy123 <1572235056@qq.com>
2024-08-01 15:38:21 +0800

    学生列表增删改查

--- Nodejs-learning-notes/13_express强化/index.js
+++ Nodejs-learning-notes/13_express强化/index.js
@@ -32,16 +32,59 @@ app.set("views", path.resolve(__dirname, "views"));
 //   { id: 2, name: "李四", age: 20, gender: "男" },
 //   { id: 3, name: "王五", age: 22, gender: "男" },
 // ];
-const STUDENTS = require("./data/students.json");
+let STUDENTS = require("./data/students.json");
 app.get("/students", (req, res) => {
   //   res.render("students", { name, age: 12, helloStr: "<h1>你好</h1>" });
   res.render("students", { students: STUDENTS });
 });
 
+app.get("/delStudent", (req, res) => {
+  console.log(req.query.id);
+  let id = +req.query.id;
+  STUDENTS = STUDENTS.filter((item) => item.id !== id);
+  fs.writeFile(
+    path.resolve(__dirname, "./data/students.json"),
+    JSON.stringify(STUDENTS)
+  )
+    .then(() => {
+      res.redirect("/students");
+    })
+    .catch(() => {
+      res.status(500);
+      res.send("删除失败");
+    });
+});
+
+app.get("/toEdit", (req, res) => {
+  const editStu = STUDENTS.find((item) => item.id === +req.query.id);
+  console.log(editStu);
+  res.render("editStudent", { stu: editStu });
+});
+
+app.post("/editStudent", (req, res) => {
+  let student = STUDENTS.find((item) => item.id === +req.body.id);
+  student.name = req.body.name;
+  student.age = req.body.age;
+  student.gender = req.body.gender;
+
+  fs.writeFile(
+    path.resolve(__dirname, "./data/students.json"),
+    JSON.stringify(STUDENTS)
+  )
+    .then(() => {
+      res.redirect("/students");
+    })
+    .catch(() => {
+      res.status(500);
+      res.send("修改失败");
+    });
+});
+
 app.post("/addStudent", (req, res) => {
   const { name, age, gender } = req.body;
+  const id = STUDENTS.at(-1) ? STUDENTS.at(-1).id + 1 : 1; // 不用length的原因：length变化太大
   const newStu = {
-    id: STUDENTS.at(-1).id + 1, // 不用length的原因：length变化太大
+    id,
     name,
     age: +age,
     gender,
@@ -70,6 +113,6 @@ app.use((req, res) => {
   res.send("<h1>404 Not Found</h1>");
 });
 
-app.listen(5000, () => {
+app.listen(5003, () => {
   console.log("服务器已启动");
 });
