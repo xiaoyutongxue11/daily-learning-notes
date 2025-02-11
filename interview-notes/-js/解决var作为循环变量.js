0391b32290e7ca157abab6003a1636912f1594c1
tyy123 <1572235056@qq.com>
2025-02-11 15:04:08 +0800

    使用闭包保留执行上下文中的变量对象

--- interview-notes/-js/解决var作为循环变量.js
+++ interview-notes/-js/解决var作为循环变量.js
@@ -4,6 +4,7 @@ for (var i = 1; i <= 5; i++) {
   }, i * 1000);
 } // 输出5个6
 
+// 使用闭包（即使函数执行结束，仍然会保留函数执行上下文的变量对象，因为始终保存的是对变量对象的引用）
 for (var i = 1; i <= 5; i++) {
   (function (j) {
     setTimeout(function timer() {
@@ -12,6 +13,7 @@ for (var i = 1; i <= 5; i++) {
   })(i);
 }
 
+// 使用setTimeout的第三个参数
 for (var i = 1; i <= 5; i++) {
   setTimeout(
     function timer(j) {
@@ -22,6 +24,7 @@ for (var i = 1; i <= 5; i++) {
   );
 }
 
+// 块级作用域
 for (let i = 1; i <= 5; i++) {
   setTimeout(function timer() {
     console.log(i);
