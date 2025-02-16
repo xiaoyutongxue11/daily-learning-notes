218c32cf1e05c9572ea5d925c6a73ab9f973455e
tyy123 <1572235056@qq.com>
2025-02-16 23:56:49 +0800

    事件循环输出

--- interview-notes/-js/promise.js
+++ interview-notes/-js/promise.js
@@ -10,11 +10,15 @@ Promise.resolve()
   })
   .then(() => {
     console.log("promise2");
+  })
+  .then(() => {
+    console.log("promise3");
   });
 
 async function async1() {
   console.log("async1 start");
   await async2();
+  process.nextTick(() => console.log("nextTick"));
   console.log("async1 end");
 }
 
